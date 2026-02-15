e7eb39272b9eff1f996217cc2d2707fa11cfc348
topthoughtsofprajeeth@gmail.com <topthoughtsofprajeeth@gmail.com>
2026-02-15 10:59:17 +0530

    modified file

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,19 @@
 
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+// ===== Emotion System =====
+enum Mood { NEUTRAL, HAPPY, ANGRY, SAD };
+Mood currentMood = NEUTRAL;
+
+unsigned long lastInteraction = 0;
+const unsigned long lonelyTime = 15000; // 15 seconds → sad
+
+// ===== Eye Settings =====
 int baseY = 16;
 int eyeHeight = 36;
 
-void drawSpiderEyes(int mood) {
+// ===== Draw Face =====
+void drawSpiderEyes(Mood mood) {
 
   display.clearDisplay();
 
@@ -20,26 +29,22 @@ void drawSpiderEyes(int mood) {
   int rx = 70;
   int y  = baseY;
 
-  // NEUTRAL
-  if (mood == 0) {
+  if (mood == NEUTRAL) {
     display.fillRoundRect(lx, y, 40, eyeHeight, 20, WHITE);
     display.fillRoundRect(rx, y, 40, eyeHeight, 20, WHITE);
   }
 
-  // HAPPY
-  if (mood == 1) {
+  else if (mood == HAPPY) {
     display.fillTriangle(lx, y+15, lx+40, y+5, lx+20, y+eyeHeight, WHITE);
     display.fillTriangle(rx+40, y+15, rx, y+5, rx+20, y+eyeHeight, WHITE);
   }
 
-  // ANGRY
-  if (mood == 2) {
+  else if (mood == ANGRY) {
     display.fillTriangle(lx, y, lx+40, y+20, lx+20, y+eyeHeight, WHITE);
     display.fillTriangle(rx+40, y, rx, y+20, rx+20, y+eyeHeight, WHITE);
   }
 
-  // SAD
-  if (mood == 3) {
+  else if (mood == SAD) {
     display.fillTriangle(lx, y+20, lx+40, y+eyeHeight, lx+20, y, WHITE);
     display.fillTriangle(rx+40, y+20, rx, y+eyeHeight, rx+20, y, WHITE);
   }
@@ -47,6 +52,31 @@ void drawSpiderEyes(int mood) {
   display.display();
 }
 
+// ===== Input System (keyboard now, sensors later) =====
+void readInput() {
+
+  if (Serial.available()) {
+    char c = Serial.read();
+
+    if (c == 'n') currentMood = NEUTRAL;
+    if (c == 'h') currentMood = HAPPY;
+    if (c == 'a') currentMood = ANGRY;
+    if (c == 's') currentMood = SAD;
+
+    lastInteraction = millis(); // remember interaction
+  }
+}
+
+// ===== Brain / Behavior =====
+void updateEmotion() {
+
+  // If no interaction for long time → sad
+  if (millis() - lastInteraction > lonelyTime) {
+    currentMood = SAD;
+  }
+}
+
+// ===== Setup =====
 void setup() {
 
   Serial.begin(115200);
@@ -58,20 +88,15 @@ void setup() {
 
   display.clearDisplay();
   display.display();
+
+  lastInteraction = millis();
 }
 
+// ===== Main Loop =====
 void loop() {
-
-  drawSpiderEyes(0);
-  delay(1200);
-
-  drawSpiderEyes(1);
-  delay(1200);
-
-  drawSpiderEyes(2);
-  delay(1200);
-
-  drawSpiderEyes(3);
-  delay(1200);
+  readInput();
+  updateEmotion();
+  drawSpiderEyes(currentMood);
+  delay(100);
 }
-
+ś
